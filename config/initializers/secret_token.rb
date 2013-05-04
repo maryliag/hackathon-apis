79d70c3ae381d70c9482c4ed1abbdd96d0d32e04
Luiz Aoqui <lgfa29@gmail.com>
2013-05-04 13:02:12 -0300

    inicio do projeto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timail::Application.config.secret_token = '32e347a50f20c55079b894a497b3ded0a9f432ad2e8eb1959ab84e3babd0705f68a3128cd4596051e1d2990bed204b6a9766ec1a938c1bdc09ae4ed16b0cf51c'
